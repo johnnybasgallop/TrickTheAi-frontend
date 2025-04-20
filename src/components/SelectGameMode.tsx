8b75b82244dbf46cbf03704e996bdc5c3e33509d
johnnybasgallop <johnnybasgallopwork@outlook.com>
2025-04-20 18:21:52 +0100

    adjusted difficulty select to work on mobile in a grid format

--- src/components/SelectGameMode.tsx
+++ src/components/SelectGameMode.tsx
@@ -6,11 +6,11 @@ interface props {
 
 const SelectGameMode: React.FC<props> = ({ onSubmit, onSelect, gameMode }) => {
   return (
-    <div className="h-full w-full flex flex-col items-center justify-center space-x-8 py-6">
+    <div className="h-full w-full flex flex-col items-center text-center justify-center md:space-x-8 py-6">
       <p className="text-lg font-semibold mb-6">
         Please select your Game Difficulty
       </p>
-      <div className="w-full flex flex-row items-start justify-center space-x-8 pt-4">
+      <div className="w-full md:flex md:flex-row items-start justify-center space-x-4 space-y-4 md:space-x-8 md:space-y-0 pt-4">
         <button
           onClick={() => onSelect("Easy")}
           className={`px-4 py-2 border-2 border-green text-green-500 font-bold text-md hover:bg-green-500 hover:text-white hover:border-green-500 ${
